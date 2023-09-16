547defd2f7a7b793882329b6e3b9a556af2d2ba6
ochosteve <ochosteve08@gmail.com>
2023-09-16 22:48:22 +0100

    add favorite page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import MovieDetail from "./Components/MovieDetails";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import Favorites from "./pages/Favorites";
+import NotFound from './pages/NotFound'
 function App() {
-
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="movies/:id" element={<MovieDetail />} />
+      <Route path="movies/:id" element={<MovieDetailsPage />} />
+      <Route path="favorites" element={<Favorites />} />
+      <Route path="*" element={<NotFound/> }/>
     </Routes>
   );
 }
 
-export default App
+export default App;
